cc81286785d97045ae99ea8f6366e7aaba316d7d
Victoires <94651953+Victoire-s@users.noreply.github.com>
2024-03-29 20:03:46 +0100

    add(feat:core of ts, and extensions)

---
+++ src/database/database.ts
@@ -0,0 +1,19 @@
+import { DataSource } from "typeorm";
+
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3307,
+    username: "your_username", // à modifier
+    password: "your_password", // à modifier
+    database: "your_database",
+    logging: true, 
+    synchronize: true,
+    entities: [
+        "src/database/entities/*.ts"
+    ],
+    migrations: [
+        "src/database/migrations/*.ts"
+    ]
+})
---
+++ src/handlers/routes.ts
@@ -0,0 +1,4 @@
+import express, { Request, Response } from "express";
+
+export const initRoutes = (app:express.Express) => { 
+}

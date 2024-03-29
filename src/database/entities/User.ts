90102adbd2938e847c60955a96f04e0e0c80b190
Victoires <94651953+Victoire-s@users.noreply.github.com>
2024-03-29 20:14:55 +0100

    add(feat: enable of metadata entity) and creation of user

---
+++ src/database/entities/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class User{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+
+    constructor(id: number, username: string, password: string){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}

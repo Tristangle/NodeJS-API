d679c15ce66afaed9f9c7fa9ae4860946450ce19
Victoires <94651953+Victoire-s@users.noreply.github.com>
2024-04-01 17:51:32 +0200

    feat(import) : import env reading extension

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { initRoutes } from "./handlers/routes";
 import { AppDataSource } from "./database/database";
+import 'dotenv/config';
 import "reflect-metadata"
 
 const main = async () => {

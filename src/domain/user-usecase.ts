6179d1916d0492124c894f19370286e9be0f9746
Victoires <94651953+Victoire-s@users.noreply.github.com>
2024-05-11 17:04:24 +0200

    feat(add): method credit / debit

--- src/domain/user-usecase.ts
+++ src/domain/user-usecase.ts
@@ -1,9 +1,18 @@
 import { DataSource } from "typeorm";
 import { User } from "../database/entities/user";
+import { AppDataSource } from "../database/database";
+import { Err } from "joi";
 
 export interface listUserFilter{
     page: number,
     result: number
+}
+
+export interface addCreditUser{
+    solde: number;
+}
+export interface debitUser{
+    solde: number;
 }
 
 export class UserUsecase{
@@ -16,5 +25,53 @@ export class UserUsecase{
         // retourner les utilisateurs
         const listeUser = await query.getMany()
         return {user:listeUser};
+    }
+
+    async addCreditUser(addCreditUser: addCreditUser, userId : number): Promise<{ user: User }> {
+        const { solde} = addCreditUser;
+
+        try {
+            // Récupérer l'utilisateur à qui ajouter le crédit
+            const userRepository = AppDataSource.getRepository(User);
+            const userToUpdate = await userRepository.findOne({ where:{id: userId}});
+
+            if (!userToUpdate) {
+                throw new Error("Utilisateur non trouvé");
+            }
+
+            // Mettre à jour le solde de l'utilisateur
+            userToUpdate.solde += solde;
+            await userRepository.save(userToUpdate);
+
+            // Renvoyer l'utilisateur mis à jour
+            return { user: userToUpdate };
+        } catch (error) {
+            throw new Error("Erreur lors de l'ajout du crédit à l'utilisateur : " + (error as Error).message);
+        }
+    }
+    async debitUser(debitUser: debitUser, userId: number): Promise<{user: User}>{
+        const { solde} = debitUser;
+
+        try {
+            // Récupérer l'utilisateur à débiter
+            const userRepository = AppDataSource.getRepository(User);
+            const userToUpdate = await userRepository.findOne({ where:{id: userId}});
+
+            if (!userToUpdate) {
+                throw new Error("Utilisateur non trouvé");
+            }
+
+            // Mettre à jour le solde de l'utilisateur
+            if(solde > userToUpdate.solde){
+                throw new Error("Argent insuffisant pour faire la transaction")
+            }
+            userToUpdate.solde -= solde;
+            await userRepository.save(userToUpdate);
+
+            // Renvoyer l'utilisateur mis à jour
+            return { user: userToUpdate };
+        } catch (error) {
+            throw new Error("Erreur lors de l'ajout du débit à l'utilisateur : " + (error as Error).message);
+        }
     }
 }
--- src/handlers/routes.ts
+++ src/handlers/routes.ts
@@ -3,9 +3,11 @@ import { UserHandler } from "./user";
 import { authMiddleware } from "./middleware/auth-middleware";
 import { roleMiddleware } from "./middleware/role-middleware";
 import { userListValidation } from "./validators/user-validator";
+import { creditUserValidation, debitUserValidation } from "./validators/argent-validator";
 import { generateValidationErrorMessage } from "./validators/generate-validation-message";
 import { UserUsecase } from "../domain/user-usecase";
 import { AppDataSource } from "../database/database";
+import { getUserIdFromToken } from "./utils/getUserId";
 export const initRoutes = (app:express.Express) => {
     app.get('/users',authMiddleware, roleMiddleware, async(req: Request, res:Response)=>{
 
@@ -29,6 +31,43 @@ export const initRoutes = (app:express.Express) => {
             console.log(error)
             res.status(500).send({ error: "Internal error" })
         }
+    });
+    // Route pour crédit solde user
+    app.patch('/credit', authMiddleware, async(req: Request, res: Response)=>{
+       const creditValidation = creditUserValidation.validate(req.body);
+       if(creditValidation.error){
+            res.status(400).send(generateValidationErrorMessage(creditValidation.error.details));
+            return;
+       }
+       const credit = creditValidation.value;
+       const userId = getUserIdFromToken(req);
+       try{
+            const userUsecase = new UserUsecase(AppDataSource);
+            const creditUser = await userUsecase.addCreditUser(credit, userId!)
+            res.status(200).json(creditUser);
+       } catch(error){
+        console.log(error);
+        res.status(500).send({error: "Internal error"});
+       }
     })
+    // Route pour retirer solde user
+    app.patch('/debit', authMiddleware, async(req: Request, res: Response)=>{
+        const debitValidation = debitUserValidation.validate(req.body);
+        if(debitValidation.error){
+             res.status(400).send(generateValidationErrorMessage(debitValidation.error.details));
+             return;
+        }
+        const debit = debitValidation.value;
+        const userId = getUserIdFromToken(req);
+        try{
+             const userUsecase = new UserUsecase(AppDataSource);
+             const debitUser = await userUsecase.debitUser(debit, userId!)
+             res.status(200).json(debitUser);
+        } catch(error:any){
+         console.log(error);
+         res.status(500).send({ error: error.message });       
+        }
+    })
+    // Route pour regarder transaction user
     UserHandler(app)
 }
